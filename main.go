ba0a13d7f80fd6c2087167614ea1adf3145609e3
Yuri Serka <yserka@hotmail.com>
2019-05-23 00:15:18 -0300

    Pra executar ($ cd ./cliente npm install && yarn && yarn build && npm start) => tem q ter o nodejs.

--- main.go
+++ main.go
@@ -1,9 +1,53 @@
 package main
 
 import (
-	"github.com/yuriserka/lpzin/routers"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+type usuario struct {
+	ID   int    `json:"id"`
+	Nick string `json:"username"`
+	Msg  string `json:"message"`
+}
+
 func main() {
-	routers.Run()
+	db := make(map[int]usuario)
+	contador := 0
+	// router := gin.Default()
+
+	// router.Use(static.Serve("/", static.LocalFile("./cliente/build", true)))
+	// api := router.Group("/api")
+	// {
+	// 	api.GET("/", func(c *gin.Context) {
+	// 		c.JSON(http.StatusOK, gin.H{
+	// 			"message": "pong",
+	// 		})
+	// 	})
+	// }
+
+	router := gin.Default()
+
+	router.POST("/teste", func(c *gin.Context) {
+		usr := usuario{
+			ID:   contador,
+			Nick: c.PostForm("nick"),
+			Msg:  c.PostForm("message"),
+		}
+		db[usr.ID] = usr
+		contador++
+		c.JSON(http.StatusOK, gin.H{
+			"usuario": usr,
+		})
+	})
+
+	router.GET("/teste", func(c *gin.Context) {
+		for _, v := range db {
+			c.Data(http.StatusOK, "text", []byte(v.Nick+"\n"))
+		}
+	})
+	router.Run(":8080")
+
+	// router.Run()
 }
--- models/users.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User struct {
-	ProfilePicture string
-}
-
-var Yuri = User{
-	ProfilePicture: "../static/img/mario.png",
-}
