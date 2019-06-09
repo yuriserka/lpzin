119fd3ebde8caa7dea39d160013e885fff8e2f7c
henrique-mariano <henrique_mariano@aluno.unb.br>
2019-06-09 02:33:15 -0300

    Deletando tudo da master pra dar o merge

--- front/src/components/Sidebar/styles.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const SidebarDiv = styled.div`
-    width: 31%;
-    height: 100%;
-    background: whitesmoke;
-`
-
-export default SidebarDiv
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/yuriserka/lpzin/api/routers"
-)
-
-func main() {
-	routers.Run()
-}
