3e776adbccc2ff6f66a98ebbf3c077eeb934f5dd
henrique-mariano <henrique_mariano@aluno.unb.br>
2019-07-05 21:35:22 -0300

    Retirando a parte comentada da tentativa de implementar a autenticação

--- api/routers/router.go
+++ api/routers/router.go
@@ -32,37 +32,10 @@ func Init(db *sql.DB) {
 	schema.CreateSchema(db)
 }
 
-// func AuthenticationRequired(auths ... string) gin.HandlerFunc {
-// 	session := sessions.Default(c)
-// 	user := session.Get("user")
-// 	if user == nil {
-// 		c.JSON(http.StatusUnauthorized, gin.H{"error": "user precisa estar cadastrado para usar os serviços"})
-// 		c.Abort()
-// 		return
-// 	}
-
-// 	if len(auths) != 0 {
-// 		authType := session.Get("authType")
-// 		if authType == nil || !funk.ContainsString(auths, authType.(string)) {
-// 			c.JSON(http.StatusForbidden, gin.H{"error": "invalid request, restricted endpoint"})
-// 			c.Abort()
-// 			return
-// 		}
-// 	}
-// }
-
 // Run roda o servidor com ele escutando na porta 8080
 func Run() {
 	router.Run(":8080")
 }
-
-// func loginRoutes(userController *controllers.UserController) {
-// 	//router.GET("/login", userController.login)
-// 	router.POST("/login/:username:senha", userController.Login)
-
-// 	router.GET("/cadastro")
-// 	router.POST("/cadastro/:username:senha")
-// }
 
 func userRoutes(userController *controllers.UserController) {
 	router.GET("/usuarios", userController.RecuperarTodosUsuarios)
